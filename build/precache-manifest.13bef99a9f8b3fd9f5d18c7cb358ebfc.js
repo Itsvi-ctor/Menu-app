b4aa88ab0ce15fed722587ab0971aa0b171d4fea
Itsvi-ctor <89270483+Itsvi-ctor@users.noreply.github.com>
2022-09-08 21:12:31 +0100

    Add files via upload

---
+++ build/precache-manifest.13bef99a9f8b3fd9f5d18c7cb358ebfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d85e84f418727d0857e56688bb8dd51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe093285e53830f9493",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe093285e53830f9493",
+    "url": "/static/js/main.2ac42d2c.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
